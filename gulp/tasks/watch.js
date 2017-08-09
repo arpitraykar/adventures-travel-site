d2f48a90ddf29a0754111c2c1f765242ed0edd99
arpit <raykararpit@gmail.com>
2017-08-09 18:33:36 +0530

    completed large hero block and intro content block.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,25 @@
+var gulp = require("gulp"),
+ gulpWatch = require("gulp-watch"),
+ browserSync = require("browser-sync").create();
+
+gulp.task('gulpWatch', function(){
+
+  browserSync.init({
+    notify : false,
+    server :{
+      baseDir :"./app"
+    }
+  })
+    gulpWatch('./app/index.html',function(){
+      browserSync.reload();
+    }),
+
+    gulpWatch('./app/assets/styles/**/*.css',function(){
+      gulp.start('cssInject');
+    })
+});
+
+gulp.task ('cssInject',['styles'], function(){
+  gulp.src('app/assets/styles/styles.css')
+  .pipe(browserSync.stream());
+});
---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+require('./gulp/tasks/styles'); //no need of .js mention when we use require.
+require('./gulp/tasks/watch');
+
+
+/*gulp.task('default', function(){
+  console.log("hey!! i have created new task!");
+});
+//gulp.task('html', function(){
+  console.log("here i am in with html task");
+});
+*/
